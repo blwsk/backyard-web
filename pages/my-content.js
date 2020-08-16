25d7f24dcf1234823cdc365afccea80041333c13
Kevin Bielawski <kbielawsk7@gmail.com>
2020-08-16 13:25:54 -0400

    sort

--- pages/my-content.js
+++ pages/my-content.js
@@ -5,9 +5,66 @@ import { gqlFetcher } from "../lib/fetcher";
 import Header from "../components/header";
 import { stripParams } from "../lib/urls";
 import Wrapper from "../components/wrapper";
-import { useState, useCallback, useRef, useEffect } from "react";
+import { useState, useCallback, useEffect } from "react";
+import { withRouter } from "next/router";
 
 const PAGE_LENGTH = 20;
+
+const sortOrderEnum = {
+  ascending: "ascending",
+  descending: "descending",
+};
+
+const getResultObject = (result) =>
+  result.allItems || result.allItemsReverseChrono;
+
+const capitalize = (s) => {
+  if (typeof s !== "string") return "";
+  const l = s.toLowerCase();
+  return l.charAt(0).toUpperCase() + l.slice(1);
+};
+
+const usePaginatedContent = ({
+  cursorValue,
+  sortOrder = sortOrderEnum.descending,
+}) => {
+  const query =
+    sortOrder === sortOrderEnum.ascending
+      ? gql`
+          query {
+            allItems(_size: ${PAGE_LENGTH}, _cursor: ${cursorValue}) {
+              data {
+                url
+                _id
+                _ts
+              }
+              before
+              after
+            }
+          }
+        `
+      : gql`
+          query {
+            allItemsReverseChrono(_size: ${PAGE_LENGTH}, _cursor: ${cursorValue}) {
+              data {
+                url
+                _id
+                _ts
+              }
+              before
+              after
+            }
+          }
+        `;
+
+  const { data, error, isValidating } = useSWR(query, gqlFetcher);
+
+  return {
+    data,
+    error,
+    isValidating,
+  };
+};
 
 const ContentPage = ({ items }) => {
   return (
@@ -41,37 +98,17 @@ const ContentPage = ({ items }) => {
   );
 };
 
-const MyContent = () => {
+const MyContent = ({ sortOrder }) => {
   const [cursor, updateCursor] = useState(null);
 
   const [pages, updatesPages] = useState([]);
 
   const cursorValue = cursor ? `"${cursor}"` : cursor;
 
-  const { data, error, isValidating } = useSWR(
-    gql`
-      query {
-        allItems(_size: ${PAGE_LENGTH}, _cursor: ${cursorValue}) {
-          data {
-            url
-            _id
-            _ts
-          }
-          before
-          after
-        }
-      }
-    `,
-    gqlFetcher
-  );
-
-  /***
-   *
-   *
-   * We need to render Page elements for each page
-   *
-   *
-   */
+  const { data, error, isValidating } = usePaginatedContent({
+    cursorValue,
+    sortOrder,
+  });
 
   useEffect(() => {
     if (pages.length === 0 && data) {
@@ -92,12 +129,46 @@ const MyContent = () => {
   }, [data, cursor]);
 
   const onLoadMoreClick = useCallback(() => {
-    updateCursor(data.data.allItems.after);
+    updateCursor(getResultObject(data.data).after);
   });
 
-  const isLoading = !data && isValidating;
+  const hasMore = data && typeof getResultObject(data.data).after === "string";
 
-  const hasMore = data && typeof data.data.allItems.after === "string";
+  return (
+    <>
+      <div>
+        {pages.map((result) => (
+          <ContentPage
+            key={`before.${getResultObject(result).before}`}
+            items={getResultObject(result).data}
+          />
+        ))}
+        {data &&
+          !isValidating &&
+          (hasMore ? (
+            <div>
+              <br />
+              <button onClick={onLoadMoreClick}>Load more</button>
+            </div>
+          ) : (
+            <div>
+              <br />
+              <div>All caught up.</div>
+            </div>
+          ))}
+      </div>
+      {isValidating && <h2>Loading...</h2>}
+      {error && <div style={{ color: "red" }}>Oops. Refresh the page.</div>}
+    </>
+  );
+};
+
+const WrappedMyContent = ({ router }) => {
+  const sortOrder = router.query.sort || sortOrderEnum.descending;
+
+  const onChangeSortOrder = useCallback((e) => {
+    router.push(`/my-content?sort=${e.target.value}`);
+  });
 
   return (
     <div>
@@ -112,33 +183,21 @@ const MyContent = () => {
           }}
         >
           <h1>My Content</h1>
+          <div>
+            <label style={{ marginRight: 8 }}>Sort:</label>
+            <select onChange={onChangeSortOrder} value={sortOrder}>
+              {Object.keys(sortOrderEnum).map((key) => (
+                <option key={key} value={key}>
+                  {capitalize(key)}
+                </option>
+              ))}
+            </select>
+          </div>
         </div>
-        <div>
-          {pages.map((result) => (
-            <ContentPage
-              key={`before.${result.allItems.before}`}
-              items={result.allItems.data}
-            />
-          ))}
-          {data &&
-            !isValidating &&
-            (hasMore ? (
-              <div>
-                <br />
-                <button onClick={onLoadMoreClick}>Load more</button>
-              </div>
-            ) : (
-              <div>
-                <br />
-                <div>All caught up.</div>
-              </div>
-            ))}
-        </div>
-        {isValidating && <h2>Loading...</h2>}
-        {error && <div style={{ color: "red" }}>Oops. Refresh the page.</div>}
+        <MyContent key={sortOrder} sortOrder={sortOrder} />
       </Wrapper>
     </div>
   );
 };
 
-export default MyContent;
+export default withRouter(WrappedMyContent);
