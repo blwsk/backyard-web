9ee3f1d27bfa4464edf01013625cc254b4940b98
Kevin Bielawski <kbielawsk7@gmail.com>
2020-08-19 09:52:43 -0400

    cleanup

--- pages/api/items.js
+++ pages/api/items.js
@@ -10,15 +10,6 @@ const items = async (req, res) => {
     res.status(400).send(null);
     return;
   }
-  //   const gqlResponse = await fetch("https://graphql.fauna.com/graphql", {
-  //     method: "POST",
-  //     body: JSON.stringify(req.body),
-  //     headers: {
-  //       Authorization: `Bearer ${secret}`,
-  //     },
-  //   });
-  //   const json = await gqlResponse.json();
-  //   res.status(gqlResponse.status).send(json);
 
   let ids = [];
 
