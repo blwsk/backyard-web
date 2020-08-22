6812d4f3bc96b86c87193a69aa03dc2bc42dfbda
Kevin Bielawski <kbielawsk7@gmail.com>
2020-08-22 12:06:40 -0400

    fetch list members

---
+++ components/listItem.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { stripParams } from "../lib/urls";
+import Link from "next/link";
+
+/**
+ *
+ * Note: this is very similar to MyContent's ContentPage component
+ *
+ */
+
+const ListItem = ({ item }) => {
+  const { _id, _ts, url } = item;
+
+  const encodedUrl = encodeURI(url);
+  const withParamsStripped = stripParams(url);
+
+  const date = new Date(_ts / 1000);
+  const dateString = date.toDateString();
+  const timeString = date.toLocaleTimeString();
+
+  return (
+    <div
+      key={_id}
+      className="p-y-3"
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+      }}
+    >
+      <div>
+        <Link href={{ pathname: "/viewer", query: { url: encodedUrl } }}>
+          <a>{withParamsStripped}</a>
+        </Link>
+        <div>
+          <small>
+            <span>{dateString}</span>
+            <span>・</span>
+            <span>{timeString}</span>
+          </small>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ListItem;
--- pages/lists.js
+++ pages/lists.js
@@ -1,6 +1,114 @@
 import Header from "../components/header";
 import Wrapper from "../components/wrapper";
 import { withRouter } from "next/router";
+import useSWR from "swr";
+import gql from "gql-tag";
+import { gqlFetcher } from "../lib/fetcher";
+import { useState } from "react";
+import ListItem from "../components/listItem";
+
+const ListDrawerContent = ({ list }) => {
+  const { _id } = list;
+
+  const { data, error } = useSWR(
+    gql`
+      query {
+        findListItemsByList(id: "${_id}") {
+          data {
+            _id
+            item {
+              url
+              _id
+              _ts
+            }
+          }
+        }
+      }
+    `,
+    gqlFetcher
+  );
+
+  if (!data) {
+    return <div>Loading...</div>;
+  }
+
+  const listItems = data.data.findListItemsByList.data;
+
+  if (listItems.length === 0) {
+    return (
+      <div>
+        <p>
+          No list members found. Add things to this list so they appear here.
+        </p>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      {listItems.map(({ item, _id: listItemId }) => {
+        return <ListItem key={listItemId} item={item} />;
+      })}
+    </div>
+  );
+};
+
+const ListDrawer = ({ list, startOpen }) => {
+  const { name } = list;
+  const [open, updateOpen] = useState(startOpen);
+
+  return (
+    <div style={{ marginBottom: 24 }}>
+      <h3
+        style={{ cursor: "pointer", display: "flex", alignItems: "center" }}
+        onClick={() => updateOpen(!open)}
+      >
+        {name}
+        <span style={{ fontSize: 16, marginLeft: 12 }}>
+          {open ? "🔽" : "🔼"}
+        </span>
+      </h3>
+      {open && <ListDrawerContent list={list} />}
+    </div>
+  );
+};
+
+const ListList = ({ openListId }) => {
+  const { data, error, isValidating } = useSWR(
+    gql`
+      query {
+        allLists {
+          data {
+            name
+            _id
+            _ts
+          }
+        }
+      }
+    `,
+    gqlFetcher
+  );
+
+  if (!data) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      {data.data.allLists.data.map((list) => {
+        return (
+          <ListDrawer
+            key={list._id}
+            list={list}
+            startOpen={
+              typeof openListId === "string" && openListId === list._id
+            }
+          />
+        );
+      })}
+    </div>
+  );
+};
 
 const Lists = ({ router }) => {
   const { id } = router.query;
@@ -10,7 +118,7 @@ const Lists = ({ router }) => {
       <Header />
       <Wrapper>
         <h1>Lists</h1>
-        <p>{id}</p>
+        <ListList openListId={id} />
       </Wrapper>
     </div>
   );
