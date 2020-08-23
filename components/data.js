d09bbd0a1862a0d1dcc046c8c0ef3c56fbab2c3c
Kevin Bielawski <kbielawsk7@gmail.com>
2020-08-23 11:51:18 -0400

    selection state

--- components/data.js
+++ components/data.js
@@ -1,6 +1,8 @@
 import useSWR from "swr";
 import { jsonParser } from "../lib/fetcher";
 import { getHostname } from "../lib/urls";
+import { useEffect, useCallback, useState } from "react";
+import { debounce } from "../lib/debounce";
 
 export const fetcher = (path, options) => {
   return fetch(path, options).then(jsonParser);
@@ -14,6 +16,61 @@ const Data = ({ url, rawUrl, renderPlaceholder }) => {
   );
 
   const { hostname, withProtocol } = getHostname(url);
+
+  const [viewportSizeKey, updateViewportSizeKey] = useState(
+    `${window.innerHeight}.${window.innerWidth}`
+  );
+
+  const [selection, updateSelection] = useState({
+    text: null,
+    htmlChunk: null,
+  });
+
+  const onSelectionChange = useCallback(
+    debounce(() => {
+      const currentSelection = document.getSelection();
+
+      if (currentSelection && !currentSelection.isCollapsed) {
+        const text = currentSelection.toString();
+        let htmlChunk;
+
+        if (
+          currentSelection.focusNode &&
+          currentSelection.focusNode.parentElement
+        ) {
+          htmlChunk = currentSelection.focusNode.parentElement.innerHTML;
+        }
+
+        updateSelection({
+          text,
+          htmlChunk,
+          anchorNode: currentSelection.anchorNode,
+        });
+      } else {
+        updateSelection({
+          text: null,
+          htmlChunk: null,
+          anchorNode: null,
+        });
+      }
+    }),
+    []
+  );
+
+  const onResize = useCallback(
+    debounce(() => {
+      updateViewportSizeKey(`${window.innerHeight}.${window.innerWidth}`);
+    }, 100)
+  );
+
+  useEffect(() => {
+    document.onselectionchange = onSelectionChange;
+    window.onresize = onResize;
+  }, []);
+
+  useEffect(() => {
+    console.log(selection);
+  }, [selection]);
 
   return (
     <div>
@@ -39,6 +96,32 @@ const Data = ({ url, rawUrl, renderPlaceholder }) => {
             className="rendered-html-body"
             dangerouslySetInnerHTML={{ __html: data.body }}
           />
+          {selection.anchorNode && (
+            <div
+              key={viewportSizeKey}
+              style={{
+                position: "absolute",
+                height: 200,
+                width: 200,
+                background: "black",
+                color: "white",
+                borderRadius: 4,
+                top: selection.anchorNode.parentElement.offsetTop - (200 + 8),
+                left:
+                  selection.anchorNode.parentElement.offsetLeft +
+                  Math.round(
+                    0.5 * selection.anchorNode.parentElement.offsetWidth
+                  ) -
+                  100,
+                display: "flex",
+                justifyContent: "center",
+                overflow: "auto",
+                padding: 8,
+              }}
+            >
+              {selection.text}
+            </div>
+          )}
         </div>
       ) : (
         renderPlaceholder()
