555f798f4c26cb3352406da5020e36b042ffc9fa
Kevin Bielawski <kbielawsk7@gmail.com>
2020-11-24 11:07:29 -0500

    landing page + form (#47)
    * landing page with form
    
    * copy

---
+++ components/index/noAuthIndex.tsx
@@ -0,0 +1,44 @@
+import Header from "../header";
+import Wrapper from "../wrapper";
+import { FORM_URL } from "../../lib/hubspotConstants";
+
+const NoAuthIndex = () => {
+  return (
+    <>
+      <Header />
+      <Wrapper className="items-center">
+        <h1 className="text-center pt-10 pb-5 md:pt-20 md:mb-20 text-orange">
+          Your digital library
+        </h1>
+        <h2 className="text-center">
+          Save anything, search everything, keep forever
+        </h2>
+        <p className="text-center mb-6 md:mb-12">
+          Subscribe to <b>newsletters</b> with a special email address. Store
+          links via <b>SMS</b>. Never lose track of what you’ve saved. It’s all
+          here, <b>instantly searchable</b>.
+        </p>
+        <a href={FORM_URL} target="_blank">
+          <button className="text-lg bg-orange text-white font-semibold py-3 px-5">
+            Request access
+          </button>
+        </a>
+      </Wrapper>
+      <style jsx>{`
+        h1 {
+          font-size: 48px;
+        }
+        h2 {
+          font-size: 24px;
+        }
+        p {
+          width: 460px;
+          max-width: 100%;
+          line-height: 1.5em;
+        }
+      `}</style>
+    </>
+  );
+};
+
+export default NoAuthIndex;
--- pages/index.tsx
+++ pages/index.tsx
@@ -10,6 +10,7 @@ import { SaveBar } from "../components/saveBar";
 import ContentPageItem from "../components/contentPageItem";
 import Link from "next/link";
 import LoadingItem from "../components/loading/LoadingItem";
+import NoAuthIndex from "../components/index/noAuthIndex";
 
 const MOST_RECENT_ITEM_LIMIT = 5;
 
@@ -65,6 +66,7 @@ const IndexPreviewContent = () => {
 const IndexWithAuth = () => {
   return (
     <>
+      <Header />
       <Wrapper className="align-center">
         <h1 className="text-center">Backyard</h1>
         <div className="my-4 w-full">
@@ -76,53 +78,11 @@ const IndexWithAuth = () => {
   );
 };
 
-const NoAuthIndex = () => {
-  return (
-    <>
-      <Wrapper className="align-center">
-        <h1 className="text-center">Backyard</h1>
-      </Wrapper>
-      <Wrapper>
-        <div className="well mb-4">
-          <h3>Save content easily</h3>
-          <p>
-            Subscribe to newsletters with a free email address. Get notified via
-            text when a new post arrives. Send links to the same phone number to
-            save them. Drag and drop files anywhere to save them.
-          </p>
-        </div>
-        <div className="well mb-4">
-          <h3>Highlight and annotate quickly</h3>
-          <p>
-            Keep track of the most important parts of an article. Record your
-            thoughts on an article with inline annotations.
-          </p>
-        </div>
-        <div className="well mb-4">
-          <h3>Search everything</h3>
-          <p>
-            Search across full article text and metadata, highlights, and notes.
-          </p>
-        </div>
-        <div className="well mb-4">
-          <h3>Organize your content into queues.</h3>
-          <p>
-            Don't worry about forgetting what you want to read next. Find a
-            piece in your queue. Split up content into queues based on content
-            type, topic, source, etc.
-          </p>
-        </div>
-      </Wrapper>
-    </>
-  );
-};
-
 const IndexComponent = requireAuth(IndexWithAuth, NoAuthIndex);
 
 const Index = () => {
   return (
     <div>
-      <Header />
       <IndexComponent />
     </div>
   );
